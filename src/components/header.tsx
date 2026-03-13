b1240a62a5954284740faf90ee81e19e196eb244
lankyjo <ikejijoshua69@gmail.com>
2026-03-13 15:46:27 +0100

    style: add blurred and semi-transparent background to scrolled header on medium screens.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -42,7 +42,7 @@ export function Header() {
 			className={cn(
 				"fixed inset-x-0 top-0 z-50 mx-auto w-full border-transparent border-b md:border md:transition-all md:ease-out",
 				{
-					"border-border max-w-3xl md:top-2":
+					"border-border max-w-3xl md:bg-background/95 md:backdrop-blur-sm md:supports-backdrop-filter:bg-background/50 md:top-2":
 						scrolled,
 				}
 			)}
